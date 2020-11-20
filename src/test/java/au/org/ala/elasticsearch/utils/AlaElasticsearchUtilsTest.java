a21767f5c316a8a5d9e3de52a15aa758c7974040
Peter Ansell <p_ansell@yahoo.com>
2020-11-20 14:23:02 +1100

    Add stub test
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

---
+++ src/test/java/au/org/ala/elasticsearch/utils/AlaElasticsearchUtilsTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package au.org.ala.elasticsearch.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link AlaElasticsearchUtils}.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+class AlaElasticsearchUtilsTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link au.org.ala.elasticsearch.utils.AlaElasticsearchUtils#asyncReindex(org.elasticsearch.client.RestHighLevelClient, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    final void testAsyncReindex() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
