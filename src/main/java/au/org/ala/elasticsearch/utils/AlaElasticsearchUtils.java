46d0d0fea9cec6bab2d68a57777f87c33106ab55
Peter Ansell <p_ansell@yahoo.com>
2020-11-20 13:09:38 +1100

    More work on reorganising
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

---
+++ src/main/java/au/org/ala/elasticsearch/utils/AlaElasticsearchUtils.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package au.org.ala.elasticsearch.utils;
+
+import java.io.IOException;
+
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksRequest;
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.tasks.TaskSubmissionResponse;
+import org.elasticsearch.index.VersionType;
+import org.elasticsearch.index.reindex.ReindexRequest;
+import org.elasticsearch.tasks.TaskInfo;
+
+/**
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class AlaElasticsearchUtils {
+
+    /**
+     * Asynchronously trigger a reindex of the given source index to the given
+     * destination index.
+     * 
+     * @param client
+     *            The {@link RestHighLevelClient} to use to reindex.
+     * @param sourceIndex
+     *            The source index.
+     * @param destinationIndex
+     *            The destination index.
+     * @return The task ID of the reindex task that was asynchronously run.
+     * @throws IOException
+     *             If communication with the server had an issue.
+     */
+    public static String asyncReindex(RestHighLevelClient client, final String sourceIndex,
+            final String destinationIndex) throws IOException {
+        // Reference:
+        // https://www.elastic.co/guide/en/elasticsearch/client/java-rest/7.10/java-rest-high-document-reindex.html
+        ReindexRequest reindexRequest = new ReindexRequest();
+        reindexRequest.setSourceIndices(sourceIndex);
+        reindexRequest.setDestIndex(destinationIndex);
+        reindexRequest.setDestVersionType(VersionType.EXTERNAL);
+        reindexRequest.setDestOpType("index");
+        // Very small sample size while creating the algorithm
+        reindexRequest.setMaxDocs(10);
+        reindexRequest.setRefresh(true);
+    
+        TaskSubmissionResponse reindexSubmission = client.submitReindexTask(reindexRequest,
+                RequestOptions.DEFAULT);
+        String taskId = reindexSubmission.getTask();
+        System.out.println(String.format("Task created with id: %s", taskId));
+        return taskId;
+    }
+
+    /**
+     * Wait for task to complete.
+     * 
+     * @param client
+     *            The {@link RestHighLevelClient} to use to check the task
+     *            status
+     * @param taskId
+     *            The task id to find and wait for completion before returning
+     * @throws InterruptedException
+     *             If waiting was interrupted.
+     * @throws IOException
+     *             If communication with the server had an issue.
+     */
+    public static void waitForTask(RestHighLevelClient client, String taskId)
+            throws InterruptedException, IOException {
+        org.elasticsearch.tasks.TaskId parentTaskId = new org.elasticsearch.tasks.TaskId(taskId);
+    
+        boolean subTasksRunning = true;
+        while (subTasksRunning) {
+            ListTasksRequest listTasksRequest = new ListTasksRequest();
+            listTasksRequest.setDetailed(true);
+            ListTasksResponse listTasksResponse = client.tasks().list(listTasksRequest,
+                    RequestOptions.DEFAULT);
+            if (listTasksResponse.getTasks().isEmpty()) {
+                subTasksRunning = false;
+            } else {
+                boolean foundMatchingRunningTask = false;
+                for (TaskInfo taskInfo : listTasksResponse.getTasks()) {
+                    if (taskInfo.getTaskId().equals(parentTaskId)) {
+                        System.out.println("Matching task: " + taskInfo.toString());
+                        foundMatchingRunningTask = true;
+                    } else {
+                        System.out.println("Non-matching task: " + taskInfo.toString());
+                    }
+                }
+                subTasksRunning = foundMatchingRunningTask;
+            }
+            if (subTasksRunning) {
+                System.out.println("Task still running, waiting again...");
+                // Wait before checking again
+                Thread.sleep(100);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/au/org/ala/elasticsearch/utils/Reindex.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package au.org.ala.elasticsearch.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+/**
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class Reindex {
+
+    public static void main(String[] args) throws Exception {
+        final OptionParser parser = new OptionParser();
+
+        final OptionSpec<Void> help = parser.accepts("help").forHelp();
+        final OptionSpec<String> sourceOption = parser.accepts("source").withRequiredArg()
+                .ofType(String.class).required().describedAs(
+                        "The elasticsearch index to use as the source for the reindex");
+        final OptionSpec<String> destinationOption = parser.accepts("destination").withRequiredArg()
+                .ofType(String.class).required().describedAs(
+                        "The elasticsearch index to use as the destination for the reindex");
+
+        OptionSet options = null;
+
+        try {
+            options = parser.parse(args);
+        } catch (final OptionException e) {
+            System.out.println(e.getMessage());
+            parser.printHelpOn(System.out);
+            throw e;
+        }
+
+        if (options.has(help)) {
+            parser.printHelpOn(System.out);
+            return;
+        }
+
+//        final String sourceIndex = "example-filebeat-nginx-2020.11.18";
+//        final String destinationIndex = "example-filebeat-www-2020.11.18";
+
+        String sourceIndex = sourceOption.value(options);
+        String destinationIndex = destinationOption.value(options);
+        
+        // Reference:
+        // https://www.elastic.co/guide/en/elasticsearch/client/java-rest/7.10/java-rest-high-getting-started-initialization.html
+        try (RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));) {
+
+            //deleteAndRecreateIndexes(client, sourceIndex, destinationIndex);
+
+            String taskId = AlaElasticsearchUtils.asyncReindex(client, sourceIndex, destinationIndex);
+
+            AlaElasticsearchUtils.waitForTask(client, taskId);
+        }
+    }
+
+}
